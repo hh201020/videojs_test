d274a8617eff7b4db8d38f1bd51347bae2bc9a7b
JerryHu <jerryhu@JerryHus-Mac-mini.local>
2017-07-01 18:12:23 -0400

    test good

--- ads/player.js
+++ ads/player.js
@@ -4,12 +4,12 @@ var options = {
   id: 'content_video',
 //  adTagUrl: 'http://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/5965368/NTD.TV_video&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&url=[referrer_url]&description_url=http://ntd.tv/&correlator=[timestamp]'
 
-  adTagUrl: 'http://pubads.g.doubleclick.net/gampad/ads?sz=640x480&' +
+/*  adTagUrl: 'http://pubads.g.doubleclick.net/gampad/ads?sz=640x480&' +
       'iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&' +
       'impl=s&gdfp_req=1&env=vp&output=xml_vmap1&unviewed_position_start=1&' +
       'cust_params=sample_ar%3Dpremidpostpod%26deployment%3Dgmf-js&cmsid=496&' +
       'vid=short_onecue&correlator='
-
+*/
 };
 
 // This must be called before player.play() below.
